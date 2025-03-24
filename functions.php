d6dda5e720268083625c6b7e3dbc8fc1a2af7e74
Yvonne Huang <yvonnehuangcanada@gmail.com>
2025-03-24 11:47:21 -0400

    Added style.css on front and back end

--- functions.php
+++ functions.php
@@ -2,8 +2,24 @@
 
 function custom_theme_enqueue_styles() {
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap', false);
+
+    // Load style.css on front-end
+    wp_enqueue_style( 
+		'school-style',
+		get_stylesheet_uri(),
+		array(),
+		wp_get_theme()->get( 'Version' ),
+		'all'
+	);
 }
 add_action('wp_enqueue_scripts', 'custom_theme_enqueue_styles');
+
+
+// Load style.css on back-end
+function school_setup() {
+	add_editor_style( get_stylesheet_uri() );
+}
+add_action( 'after_setup_theme', 'school_setup' );
 
 /**
 * Custom Post Types & Custom Taxonomies
