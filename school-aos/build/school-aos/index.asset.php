be39de7fb0c09dfc9f3a5d879786a104007ec451
Yvonne Huang <yvonnehuangcanada@gmail.com>
2025-04-01 17:42:07 -0400

    Updated styles

--- school-aos/build/school-aos/index.asset.php
+++ school-aos/build/school-aos/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ca08e3d95d4992578885');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '873dcd1299de196b13b0');
